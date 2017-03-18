a0b7b2c90c8ab55902eec7ce61aee759cdf897ef
olive <olive@Danilo-Pc>
2017-03-18 16:56:24 -0300

    Classe para envio de email pronta

--- src/br/cnec/fcsl/sistema/Principal.java
+++ src/br/cnec/fcsl/sistema/Principal.java
@@ -3,8 +3,10 @@ package br.cnec.fcsl.sistema;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		SendMail sm = new SendMail("smtp.gmail.com","465");
+		sm.sendMail("oliveira.daniloneri@gmail.com","cristianhenrique06@hotmail.com","Teste","Envio de email usando a API JavaMail");
+		
 	}
 
 }
